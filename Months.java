a1f71db70018211be4bd3dbb3294fe3d7f865e74
Katarzyna Krakowska <48708231+234567812@users.noreply.github.com>
2019-04-14 21:43:50 +0100

    Update Months.java

--- Months.java
+++ Months.java
@@ -1,9 +1,13 @@
-
+/*
+Name: Exercise_KKrakowska
+Description: Exercise - Java Development
+Created by: Katarzyna Krakowska
+Created on: 14/04/2019
+*/
 
 public class Months{
 
 	public static void main(String[] args){
-
 	
 	String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 	
