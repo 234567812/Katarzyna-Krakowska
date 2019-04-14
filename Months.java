4aa96e20b8e81889c523244d025bb67ca3069e74
Katarzyna Krakowska <48708231+234567812@users.noreply.github.com>
2019-04-14 21:42:35 +0100

    Update Months.java

--- Months.java
+++ Months.java
@@ -1,12 +1,19 @@
+
+
 public class Months{
 
 	public static void main(String[] args){
 
-		
+	
 	String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 	
 	for (int i = 0; i < months.length; i++) {
-        System.out.println(months[i] + " ");
-	}
+       
+        if(months[i].contains ("r")){
+	System.out.println (months[i]);
+	
+	}//end if
+	}//end for
+	
 	}//end main
 }//end class
