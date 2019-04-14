92074eebe61c85c26ac4f47178b2c7279ba2fdc2
Katarzyna Krakowska <48708231+234567812@users.noreply.github.com>
2019-04-14 15:03:23 +0100

    Update Months.java

--- Months.java
+++ Months.java
@@ -3,21 +3,10 @@ public class Months{
 	public static void main(String[] args){
 
 		
-	String[] subjects = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-	String search = "r";
-
-		search = "r";
-		boolean found = false;
-
-		for (int i = 0; i < subjects.length; i++){
-			if (subjects[i].equals(search)){
-			System.out.println("Subjects found. Stored in index "+i);
-			found = true;
-			}
-		}
-		if(!found) {
-			System.out.println("Subject not found.");
-			}
-
+	String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+	
+	for (int i = 0; i < months.length; i++) {
+        System.out.println(months[i] + " ");
+	}
 	}//end main
 }//end class
